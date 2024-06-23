14df5cc506f873f1b65d4f1dbb3db52bfccc0c2c
Van-Nice <wilsonwalter808@gmail.com>
2024-06-23 07:50:43 -0600

    Get list of all 1025 pokemon in txt file

--- Main.php
+++ Main.php
@@ -1,1 +1,13 @@
 <?php
+require 'vendor/autoload.php';
+use Goutte\Client;
+$client = new Client();
+
+$url = "https://pokemondb.net/pokedex/all";
+$crawler = $client->request('GET', $url);
+
+$crawler->filter('div > div > p')->each(function ($node) {
+  print $node->text()."\n";
+});
+
+
